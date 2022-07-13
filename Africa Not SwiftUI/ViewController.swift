f56387c8c5264f6ad29f41479445109ac58742ad
Dan Mori <danielstelzer@gmail.com>
2022-07-13 19:59:06 -0300

    Initial Commit

---
+++ Africa Not SwiftUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Africa Not SwiftUI
+//
+//  Created by Dan Mori on 13/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
